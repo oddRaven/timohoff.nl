f63ddf59f2c1f1e3bdb4937e13db85ebd265abc7
Timo Hoff <timohoff@xxxxxs-MacBook-Pro.local>
2024-01-05 21:51:35 +0100

    add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { AboutComponent } from './about/about.component';
 import { TeamComponent } from './team/team.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PassionsComponent } from './passions/passions.component';
 import { WebsiteComponent } from './website/website.component';
-import { LanguageSelectorComponent } from './language-selector/language-selector.component';
-import { NoteDirective } from './note/note.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent, AboutComponent, TeamComponent, SkillsComponent, PassionsComponent, WebsiteComponent, LanguageSelectorComponent, NoteDirective],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MenuComponent, AboutComponent, TeamComponent, SkillsComponent, PassionsComponent, WebsiteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
+import { NoteDirective } from '../note/note.directive';
+
+@Component({
+  selector: '[app-header]',
+  standalone: true,
+  imports: [LanguageSelectorComponent, NoteDirective],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
