71aaf7cb00bc9c8af64cfbd7180ca8fa285f0d06
Timo Hoff <timohoff@xxxxxs-MacBook-Pro.local>
2024-01-09 21:18:40 +0100

    add menu pop-up for mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
-import { CommonModule, isPlatformServer } from '@angular/common';
+import { CommonModule, isPlatformServer, DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
+import { PopUpComponent } from './pop-up/pop-up.component';
 import { MenuComponent } from './menu/menu.component';
 import { AboutComponent } from './about/about.component';
 import { TeamComponent } from './team/team.component';
@@ -14,7 +15,7 @@ import { ScrollActiveDirective } from './scroll-active/scroll-active.directive';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, MenuComponent, AboutComponent, TeamComponent, SkillsComponent, PassionsComponent, WebsiteComponent, ScrollActiveDirective],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, PopUpComponent, MenuComponent, AboutComponent, TeamComponent, SkillsComponent, PassionsComponent, WebsiteComponent, ScrollActiveDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -23,7 +24,8 @@ export class AppComponent implements OnInit {
   offset: number = 0;
 
   constructor (
-    @Inject(PLATFORM_ID) private platformId: Object)
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private document: Document)
   {
   }
 
@@ -39,5 +41,13 @@ export class AppComponent implements OnInit {
     if (window.innerWidth <= 500) {
       this.offset = 40;
     }
+  }
+
+  closePopUp () {
+    this.document.body.classList.remove('overflow-hidden');
+    let container = this.document.querySelector('.container');
+    container?.classList.remove('disabled');
+    let popUp = this.document.querySelector('app-pop-up');
+    popUp?.classList.remove('active');
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
 import { NoteDirective } from '../note/note.directive';
@@ -13,4 +14,17 @@ import { NoteDirective } from '../note/note.directive';
 export class HeaderComponent {
   wipNote = $localize`:@@wipNote:wip`
   flagsNote = $localize`:@@flagsNote:flags`
+
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document)
+  {}
+
+  openMobileMenu () {
+    this.document.body.classList.add('overflow-hidden');
+    let container = this.document.querySelector('.container');
+    container?.classList.add('disabled');
+    let popUp = this.document.querySelector('app-pop-up');
+    popUp?.classList.add('active');
+  }
 }
