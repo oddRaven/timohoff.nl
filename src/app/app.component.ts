8ee51290b53ccfff37182ef176d188b929cb9a11
Timo Hoff <timohoff@xxxxxs-MacBook-Pro.local>
2024-01-18 14:43:30 +0100

    add contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,13 @@ import { TeamComponent } from './team/team.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PassionsComponent } from './passions/passions.component';
 import { WebsiteComponent } from './website/website.component';
+import { ContactComponent } from './contact/contact.component';
 import { ScrollActiveDirective } from './scroll-active/scroll-active.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, PopUpComponent, MenuComponent, AboutComponent, TeamComponent, SkillsComponent, PassionsComponent, WebsiteComponent, ScrollActiveDirective],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, PopUpComponent, MenuComponent, AboutComponent, TeamComponent, SkillsComponent, PassionsComponent, WebsiteComponent, ContactComponent, ScrollActiveDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
